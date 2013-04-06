b23e7fedac24893749058d3d4feb9330c4197d97
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-04-06 11:31:23 -0600

    0.0.5

--- js/actions.js
+++ js/actions.js
@@ -18,6 +18,33 @@ function (compassError) {
 			$('#bRes').text(0.000);
 		});
 
+		//Acelerometro
+		var awatchID = null;
+		$('#aInc').tap(function(){
+			 awatchID = navigator.accelerometer.watchAcceleration(function (acceleration){
+				 $('#aRes').html('Acceleration X: ' + acceleration.x + '<br />' +
+                            	 'Acceleration Y: ' + acceleration.y + '<br />' +
+                           		 'Acceleration Z: ' + acceleration.z + '<br />' +
+                            	 'Timestamp: '      + acceleration.timestamp + '<br />');
+			 },
+function (accError) {
+        alert('Accelerelometer error: ' + accError.code);
+},
+
+	{ frequency: 500 });// Update every 3 seconds
+		});
+		$('#aDtn').tap(function(){
+			navigator.accelerometer.clearWatch(awatchID);
+			awatchID = null;
+			$('#aRes').text('Detenido');
+		});
+		
+		//Globalización
+		navigator.globalization.getPreferredLanguage(function (language){
+			 $('#gRes').text('language: ' + language.value + '\n');
+				},function (){
+			   alert('Error getting language\n');
+	   });		
 
 	},false);
 	
